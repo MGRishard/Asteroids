2a8f8c95320c90ae825e3500bd6ddce9d224722a
MGRishard <marcin.jakub.gorny@gmail.com>
2026-04-11 18:18:13 +0200

    2.4 - FTP

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ import pygame
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dv = 0
     while True:
         log_state()
         for event in pygame.event.get():
             pass
         screen.fill("black")
         pygame.display.flip()
+        dv = clock.tick(60)/1000
+        print(dv)
     print("Starting Asteroids with pygame version: 2.6.1")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
