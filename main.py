94252e9b75fa4f7d1509ae81e80768bd3188690d
MGRishard <marcin.jakub.gorny@gmail.com>
2026-04-11 18:08:47 +0200

    2.3 - Adding screen

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+from logger import log_state
 from constants import *
 import pygame
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            pass
+        screen.fill("black")
+        pygame.display.flip()
     print("Starting Asteroids with pygame version: 2.6.1")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
