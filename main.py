61efba049d8119f2c9b3de70c192c8e5a27a37f7
MGRishard <marcin.jakub.gorny@gmail.com>
2026-04-12 16:26:05 +0200

    3.2 - Player Draw

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
+import pygame
+from player import Player
 from logger import log_state
 from constants import *
-import pygame
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dv = 0
     while True:
         log_state()
         for event in pygame.event.get():
             pass
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dv = clock.tick(60)/1000
   #      print(dv)
